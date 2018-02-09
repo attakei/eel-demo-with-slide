29bd847cd10e7b71a6dcf7198f8b6704e248c104
Kazuya Takei <attakei@gmail.com>
2018-02-09 11:26:29 +0900

    Use python-whois

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import eel
+from whois import whois
+
+@eel.expose
+def get_whois(zone):
+    print(f"Whois {zone}")
+    w = whois(zone)
+    w_str = '\n'.join([f"{k}:\t{v}" for k, v in w.items()])
+    print(w_str)
+    return w_str
 
 
 eel.init('web')
